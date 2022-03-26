5770ef2bf57e76cbb312aadc33bd1c4036d6158c
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-03-26 18:47:52 +0700

    JS Testing

---
+++ Dasar JS/jstesting/index.js
@@ -0,0 +1,10 @@
+// materi.js;
+// !Manual testing Proses pengujian secara manual oleh seorang yang ditunjuk sebagai test, atau sebagian pengguna yang memang mendapatkan akses untuk pengujian pre-release. Proses ini biasanya berkaitan dengan usability, accessibility dari sebuah aplikasi.
+
+// !Otomatis Proses pengujian secara otomatis dilakukan oleh komputer dengan menuliskan script khusus, biasanya dilakukan oleh software engineer langsung ataupun oleh seorang QA Engineer. Proses ini biasanya berkaitan dengan fungsionalitas dari sebuah aplikasi.
+
+// !Jenis otomatic testing
+// *1. Static testing memastikan bahwa sebuah aplikasi tidak mengalami kesalahan secara fisik.
+// *2. Unit testing dilakukan untuk memastikan bahwa setiap unit kode yang kita tulis sudah bisa bekerja sesuai harapan.
+// *3. Integration testing memastikan seluruh unit kode yang kita tulis sudah bisa bekerja sesuai harapan.
+// *4 End-to-end testing menguji seluruh proses dari awal hingga akhir.
