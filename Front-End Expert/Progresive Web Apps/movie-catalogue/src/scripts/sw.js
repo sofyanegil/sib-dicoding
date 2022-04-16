dd4850e9daeedb70c860699754792b2b98cb80e8
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2022-04-16 08:02:12 +0700

    Progressive Web Apps

---
+++ Front-End Expert/Progresive Web Apps/movie-catalogue/src/scripts/sw.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-restricted-globals */
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (!(event.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
+
+  event.respondWith(CacheHelper.revalidateCache(event.request));
+});
